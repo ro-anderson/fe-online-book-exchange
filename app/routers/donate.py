498117da0d6d243f0dd7253972fa3b233b511253
Rodrigo Anderson <rodrigo.anderson@emcasa.com>
2023-05-28 22:26:20 -0300

    feat add BASE_URL (prod and dev) and update requirements.txt

--- app/routers/donate.py
+++ app/routers/donate.py
@@ -3,10 +3,13 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import requests
 import httpx
+import os
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates/")
 
+# Get the base URL from environment variable or use a default value
+BASE_URL = os.environ.get('BASE_URL', 'http://0.0.0.0:5001')
 
 @router.get("/donate", response_class=HTMLResponse)
 def get_donate(request: Request):
@@ -22,7 +25,7 @@ async def post_donate(request: Request, nome: str = Form(...), email: str = Form
     }
 
     async with httpx.AsyncClient() as client:
-        response = await client.post("http://0.0.0.0:5001/api/users", json=user_data)
+        response = await client.post(f"{BASE_URL}/api/users", json=user_data)
         if response.status_code == 200:
             print(f"\nresponse: {response.json()}\n")
             user_id = response.json()["data"]["id"]
@@ -33,7 +36,7 @@ async def post_donate(request: Request, nome: str = Form(...), email: str = Form
                     "user_id": user_id
                 },
             }
-            response = await client.post("http://0.0.0.0:5001/api/donations", json=donation_data)
+            response = await client.post("{BASE_URL}/api/donations", json=donation_data)
             success = response.status_code == 200
         else:
             success = False
